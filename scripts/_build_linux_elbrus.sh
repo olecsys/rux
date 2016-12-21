1702a6fd035e5b2d351a766c78735e136bab53ac
olecsys <saneksamara@mail.ru>
2016-12-21 16:12:24 +0400

    Fix compile elbrus

--- scripts/_build_linux_elbrus.sh
+++ scripts/_build_linux_elbrus.sh
@@ -4,6 +4,7 @@ old_dir=`dirname "$old_dir"`
 cd "`dirname "$0"`"
 script_dir="`pwd`/`basename "$0"`"
 script_dir=`dirname "$script_dir"`
+cd "$old_dir"
 
 os=linux
 platform=elbrus
@@ -12,17 +13,53 @@ rebuild_success="true"
 if [ $rebuild_success = "true" ]
 then	
 	rebuild_success="false"
-	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --rebuild-project "$script_dir/../rux.framework/build.ruxprj" --configuration "$os $platform" && rebuild_success="true"
+	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --generate-makefile "$script_dir/../rux.framework/build.ruxprj" && rebuild_success="true"
 fi
 if [ $rebuild_success = "true" ]
 then	
 	rebuild_success="false"
-	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --rebuild-project "$script_dir/../rux.framework/test/test.ruxprj" --configuration "$os $platform" && rebuild_success="true"
+	cd "$script_dir/../projects/$os/$platform/rux.framework" && rebuild_success="true"
 fi
 if [ $rebuild_success = "true" ]
 then	
 	rebuild_success="false"
-	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --rebuild-project "$script_dir/../rux.engine/build.ruxprj" --configuration "$os $platform" && rebuild_success="true"
+	make -j 4 -f Makefile && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	cd "$old_dir" && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --build-project "$script_dir/../rux.framework/build.ruxprj" --configuration "$os $platform" && rebuild_success="true"
+fi
+
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --generate-makefile "$script_dir/../rux.engine/build.ruxprj" && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	cd "$script_dir/../projects/$os/$platform/rux.engine" && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	make -j 4 -f Makefile && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	cd "$old_dir" && rebuild_success="true"
+fi
+if [ $rebuild_success = "true" ]
+then	
+	rebuild_success="false"
+	"$script_dir/../tools/rux.configure/$os/x64/rux.configure" --build-project "$script_dir/../rux.engine/build.ruxprj" --configuration "$os $platform" && rebuild_success="true"
 fi
 red='\e[0;31m'
 green='\e[0;32m'
